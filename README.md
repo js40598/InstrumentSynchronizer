23028c3bec5d5c86632f2c001167dca924750a38
unknown <szymonjarzab1997@gmail.com>
2020-09-18 03:29:08 +0200

    Metronome class added

--- README.md
+++ README.md
@@ -2,17 +2,15 @@
 
 Done: 
     file converter, 
-    spectrum extracter, 
+    spectrum extractor, 
     BPM detector, 
-    read_audio_samples gets 2 separate tables and has to be more efficient (.T)
+    read_audio_samples gets 2 separate tables and has to be more efficient (.T),
+    metronome generator,
+    Get more samples to try out if BPM detector works well, 
 
 To do: 
     Try different win_s and hop_s and create sets to make app more efficient, 
-    Get more samples to try out if BPM detector works well, 
     Go on with app functions, not only reading files,
     Beats coords saved in tab and used to make them synchronize with others? 
-    Metronome generator?
     Tests
-    
-    
     
--- instrsyn/AudioFile.py
+++ instrsyn/AudioFile.py
@@ -10,6 +10,9 @@ class AudioFile:
     def __init__(self, directory):
         self.directory = directory
         self.format = directory.split('.')[-1]
+        self.audio = self.read_audio()
+        self.samplesleft, self.samplesright = self.read_audio_samples()
+        self.framerate = self.audio[0]
 
     def convert(self, goto):
         exto = os.path.splitext(self.directory)[0] + '.' + goto
@@ -30,16 +33,16 @@ class AudioFile:
         self.directory[-3:] = self.format
         self.directory = ''.join(list(self.directory))
 
-    def read_audio_samples(self):
+    def read_audio(self):
         if self.format != 'wav':
             self.convert('wav')
-        self.audio = read(self.directory)
-        self.framerate = self.audio[0]
+        return read(self.directory)
+
+    def read_audio_samples(self):
         if isinstance(self.audio[1][0], list):
-            self.samplesleft, self.samplesright = self.audio[1].T
+            return self.audio[1].T
         else:
-            self.samplesleft = self.audio[1]
-            self.samplesright = self.audio[1]
+            return self.audio[1], self.audio[1]
 
     def display_plot(self, start_at_second, end_at_second):
         start_at_second = round_seconds_by_frequency(self.framerate, start_at_second)
@@ -59,43 +62,44 @@ class AudioFile:
         plt.title("Sample Wav")
         plt.show()
 
-    def detect_notes(self):
-        #self.sampleblocks = np.array(self.audio[1])
-        possible_bpm = [200, 183, 164, 129, 112, 90, 79, 65, 57, 52, 51, 45, 35]
-        possible_bps = [x/60 for x in possible_bpm]
-        total_beats = possible_bps[0] * len(self.audio[1]) / self.framerate
-        total_full_beats = int(total_beats)
-        holder = []
-        print(len(self.audio[1]), 'here')
-        print(total_beats)
-        for i in range(len(self.audio[1]), int((total_full_beats * self.framerate) / possible_bps[0]), -1):
-            holder.append(i)
-            self.audio[1][:-1]
-        print(self.audio[1].reshape(total_full_beats, -1))
 
-    def detect_tempo(self):
-        count = 0
-        length = len(self.samplesleft)
-        numOfWin = length / (43 * 1024)
-        numOfWin = int(numOfWin)
-        newNumSamp = numOfWin * (43 * 1024)
-        yLeft = self.samplesleft[0:newNumSamp]
-        yRight = self.samplesright[0:newNumSamp]
-        LReshapedData = np.array(yLeft).reshape(1024, numOfWin * 43)
-        RReshapedData = np.array(yRight).reshape(1024, numOfWin * 43)
-        EWin = np.zeros(43)
-        for i in range(43, 0, -1):
-            Eseg = sum(np.power(LReshapedData[:, i], 2) + np.power(RReshapedData[:, i], 2))
-            EWin[43-i] = Eseg
-        for i in range(43, numOfWin * 43):
-            Eseg = sum(np.power(LReshapedData[:, i], 2) + np.power(RReshapedData[:, i], 2))
-            avgE = sum(EWin) / 43
-            VarE = sum(np.power(np.add(EWin, avgE), 2)) / 43
-            C = (-0.0025714 * VarE) + 1.5142857
-            if EWin[0] > abs(C)*avgE:
-                count += 1
-            EWin = np.append(EWin[1:42], Eseg)
-        tsec = newNumSamp / self.framerate
-        tmin = tsec / 60
-        numBPM = count/tmin
-        print(numBPM)
+#     def detect_notes(self):
+#         #self.sampleblocks = np.array(self.audio[1])
+#         possible_bpm = [200, 183, 164, 129, 112, 90, 79, 65, 57, 52, 51, 45, 35]
+#         possible_bps = [x/60 for x in possible_bpm]
+#         total_beats = possible_bps[0] * len(self.audio[1]) / self.framerate
+#         total_full_beats = int(total_beats)
+#         holder = []
+#         print(len(self.audio[1]), 'here')
+#         print(total_beats)
+#         for i in range(len(self.audio[1]), int((total_full_beats * self.framerate) / possible_bps[0]), -1):
+#             holder.append(i)
+#             self.audio[1][:-1]
+#         print(self.audio[1].reshape(total_full_beats, -1))
+#
+#     def detect_tempo(self):
+#         count = 0
+#         length = len(self.samplesleft)
+#         numOfWin = length / (43 * 1024)
+#         numOfWin = int(numOfWin)
+#         newNumSamp = numOfWin * (43 * 1024)
+#         yLeft = self.samplesleft[0:newNumSamp]
+#         yRight = self.samplesright[0:newNumSamp]
+#         LReshapedData = np.array(yLeft).reshape(1024, numOfWin * 43)
+#         RReshapedData = np.array(yRight).reshape(1024, numOfWin * 43)
+#         EWin = np.zeros(43)
+#         for i in range(43, 0, -1):
+#             Eseg = sum(np.power(LReshapedData[:, i], 2) + np.power(RReshapedData[:, i], 2))
+#             EWin[43-i] = Eseg
+#         for i in range(43, numOfWin * 43):
+#             Eseg = sum(np.power(LReshapedData[:, i], 2) + np.power(RReshapedData[:, i], 2))
+#             avgE = sum(EWin) / 43
+#             VarE = sum(np.power(np.add(EWin, avgE), 2)) / 43
+#             C = (-0.0025714 * VarE) + 1.5142857
+#             if EWin[0] > abs(C)*avgE:
+#                 count += 1
+#             EWin = np.append(EWin[1:42], Eseg)
+#         tsec = newNumSamp / self.framerate
+#         tmin = tsec / 60
+#         numBPM = count/tmin
+#         print(numBPM)
