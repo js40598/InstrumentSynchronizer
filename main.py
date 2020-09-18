fd3ced46ee541cc8a809064df36d7c297d16dc05
unknown <szymonjarzab1997@gmail.com>
2020-09-18 03:29:53 +0200

    .gitignore, main.py with commands explained

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from scipy.io.wavfile import read
+from scipy.io.wavfile import read, write
+from instrsyn.Metronome import Metronome
 import matplotlib.pyplot as plt
 import numpy as np
 import time
@@ -7,36 +8,39 @@ from instrsyn.round_seconds_by_frequency import round_seconds_by_frequency
 from instrsyn.bpm_detection import get_file_bpm
 import os
 
-samples = {
-    '45': 'samples/metronome/45.mp3',
-    '52': 'samples/metronome/52.mp3',
-    '57': 'samples/metronome/57.mp3',
-    '61': 'samples/metronome/61.mp3',
-    '65': 'samples/metronome/65.mp3',
-    '79': 'samples/metronome/79.mp3',
-    '90': 'samples/metronome/90.mp3',
-    '112': 'samples/metronome/112.mp3',
-    '129': 'samples/metronome/129.mp3',
-    '164': 'samples/metronome/164.mp3',
-    '183': 'samples/metronome/183.mp3',
-    '200': 'samples/metronome/200.mp3',
-}
-test_file_directory = 'tests/unit/unittest_samples/45.mp3'
 
-a = AudioFile(test_file_directory)
-a.convert('wav')
-for key, value in samples.items():
-    start_time = time.time()
+# # Metronome(frequency, duration_seconds, bpm)
+# # creates metronome object
+# # write('file/directory', frequency, array_of_samples)
+# # selves: bpm, frequency, duration, samples
 
-    a = AudioFile(value)
-    a.read_audio_samples()
-    a.display_plot(0, 1)
-    bpm = get_file_bpm(a.directory, params={'win_s': 128,
-                                            'hop_s': 32,
-                                            'samplerate': a.framerate
-                                            })
-    print('BPM expected: ', key, '\nBPM detected: ', bpm)
-    print("--- %s seconds ---" % (time.time() - start_time))
+m = Metronome(44100, 10, 120)
+write('samples/metronome/generated_metronome.wav', m.frequency, m.samples)
 
 
+# # AudioFile('file/directory')
+# # creates audio file object
+# # methods:
+# # convert(goto) gotos: mp3, wav, flv, raw, ogg
+# # read_audio_samples() change format if not wav,
+# # reads file framerate and samples in stereo if possible
+# # display_plot(start_at_second, end_at_second)
+# # detect_notes(), detect_tempo() does not work
+# # selves: directory, format, audio, framerate, samplesleft, samplesright
+
+a = AudioFile('samples/metronome/generated_metronome.wav')
+a.read_audio_samples()
+a.display_plot(0, 5)
+
+
+
+# # get_file_bpm(path, params = None) params={'win_s': int,          # win_s
+# #                                           'hop_s': int,          # h
+# #                                           'samplerate': int})    # sample
+# # return bpm of file (float)
+bpm = get_file_bpm(a.directory, params={'win_s': 128,
+                                        'hop_s': 32,
+                                        'samplerate': a.framerate
+                                        })
+print(bpm)
 
--- tests/unit/audiofile_test.py
+++ tests/unit/audiofile_test.py
@@ -6,13 +6,13 @@ import definitions
 
 class AudioFileTest(TestCase):
     def test_create_audio_file(self):
-        test_file_directory = 'samples/testsamples/45.mp3'
+        test_file_directory = os.path.join('unittest_samples', 'generated_metronome.mp3')
         a = AudioFile(test_file_directory)
         self.assertEqual(a.directory, test_file_directory)
         self.assertEqual(a.format, 'mp3')
 
     def test_convert_audio_file(self):
-        test_file_directory = os.path.join('unittest_samples', '45.mp3')
+        test_file_directory = os.path.join('unittest_samples', 'generated_metronome.mp3')
         a = AudioFile(test_file_directory)
         a.convert('flv')
         self.assertEqual(a.format, 'flv')
@@ -20,13 +20,13 @@ class AudioFileTest(TestCase):
         self.assertEqual(a.format, 'ogg')
         a.convert('mp3')
         self.assertEqual(a.format, 'mp3')
-#        a.convert('raw')
-#        self.assertEqual(a.format, 'raw')
+#         a.convert('raw')
+#         self.assertEqual(a.format, 'raw')
         a.convert('wav')
         self.assertEqual(a.format, 'wav')
-        os.remove(os.path.join('unittest_samples', '45.flv'))
-        os.remove(os.path.join('unittest_samples', '45.ogg'))
-#        os.remove(os.path.join('unittest_samples', '45.raw'))
-        os.remove(os.path.join('unittest_samples', '45.wav'))
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.flv'))
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.ogg'))
+#        os.remove(os.path.join('unittest_samples', 'generated_metronome.raw'))
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.wav'))
 
 
