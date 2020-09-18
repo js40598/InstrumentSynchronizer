a4a6a6393ad3c14b07941c37e3e258b7861a8710
unknown <szymonjarzab1997@gmail.com>
2020-09-18 06:42:50 +0200

    audiofile test corrected, setup and teardown added

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ Done:
     read_audio_samples gets 2 separate tables and has to be more efficient (.T),
     metronome generator,
     Get more samples to try out if BPM detector works well, 
+    Try different win_s and hop_s and create sets to make app more efficient,
+    ^(256, 16 works for bpm 60-180)
 
-To do: 
-    Try different win_s and hop_s and create sets to make app more efficient, 
+To do:  
+    Find more efficient way to find bpm,
     Go on with app functions, not only reading files,
     Beats coords saved in tab and used to make them synchronize with others? 
     Tests
--- tests/unit/audiofile_test.py
+++ tests/unit/audiofile_test.py
@@ -1,18 +1,30 @@
 from unittest import TestCase
 from instrsyn.AudioFile import AudioFile
+from instrsyn.Metronome import Metronome
+from scipy.io.wavfile import write
 import os
-import definitions
 
 
 class AudioFileTest(TestCase):
+    def setUp(self):
+        m = Metronome(44100, 10, 60)
+        write(os.path.join('unittest_samples', 'generated_metronome.wav'), m.frequency, m.samples)
+
+    def tearDownClass(self=None):
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.flv'))
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.ogg'))
+#         os.remove(os.path.join('unittest_samples', 'generated_metronome.raw'))
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.wav'))
+        os.remove(os.path.join('unittest_samples', 'generated_metronome.mp3'))
+
     def test_create_audio_file(self):
-        test_file_directory = os.path.join('unittest_samples', 'generated_metronome.mp3')
+        test_file_directory = os.path.join('unittest_samples', 'generated_metronome.wav')
         a = AudioFile(test_file_directory)
         self.assertEqual(a.directory, test_file_directory)
-        self.assertEqual(a.format, 'mp3')
+        self.assertEqual(a.format, 'wav')
 
     def test_convert_audio_file(self):
-        test_file_directory = os.path.join('unittest_samples', 'generated_metronome.mp3')
+        test_file_directory = os.path.join('unittest_samples', 'generated_metronome.wav')
         a = AudioFile(test_file_directory)
         a.convert('flv')
         self.assertEqual(a.format, 'flv')
@@ -22,11 +34,8 @@ class AudioFileTest(TestCase):
         self.assertEqual(a.format, 'mp3')
 #         a.convert('raw')
 #         self.assertEqual(a.format, 'raw')
+        print('now')
         a.convert('wav')
         self.assertEqual(a.format, 'wav')
-        os.remove(os.path.join('unittest_samples', 'generated_metronome.flv'))
-        os.remove(os.path.join('unittest_samples', 'generated_metronome.ogg'))
-#        os.remove(os.path.join('unittest_samples', 'generated_metronome.raw'))
-        os.remove(os.path.join('unittest_samples', 'generated_metronome.wav'))
 
 
